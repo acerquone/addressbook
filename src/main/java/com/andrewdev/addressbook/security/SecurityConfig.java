3fb263b6522ae6e60804006cfe6441cfe006c9d1
acerquone <andres.cerquone@gmail.com>
2024-04-19 20:23:46 -0300

    Spring-Security-Custom-Login

---
+++ src/main/java/com/andrewdev/addressbook/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.andrewdev.addressbook.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated())
+                .formLogin(form -> form.loginPage("/showLoginPage")
+                        .loginProcessingUrl("/authenticateUser")
+                        .permitAll());
+
+        return httpSecurity.build();
+    }
+}
