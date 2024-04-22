24f7cc8f9d4838263dd7f2418d266fadc3fa971c
acerquone <andres.cerquone@gmail.com>
2024-04-21 21:41:06 -0300

    Spring-Security-JDBC-authentication Login-page-style

--- src/main/java/com/andrewdev/addressbook/security/SecurityConfig.java
+++ src/main/java/com/andrewdev/addressbook/security/SecurityConfig.java
@@ -3,10 +3,20 @@ package com.andrewdev.addressbook.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
+
+    // Standard Spring Security tables
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
